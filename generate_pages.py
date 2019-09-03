bc656f6fed3709e71626fd498ed0c7454d1aa710
Helios41 <jonny41@msn.com>
2019-09-03 11:43:28 -0400

    Generator works

--- generate_pages.py
+++ generate_pages.py
@@ -1,13 +1,51 @@
-def htmlNavBarTab(text, is_selected, style):
-   result =  '<div style="' + style + '" class="nav_tab_div ' + ("nav_tab_selected" if is_selected else "nav_tab_not_selected") + '" onclick="">\n'
+#Utils
+def readFile(path):
+   f = open(path, "r")
+   result = f.read()
+   f.close()
+   return result
+
+def writeFile(path, text):
+   f = open(path, "w")
+   f.write(text)
+   f.close()
+
+def prettyIndentation(template, replace_text, raw_text):
+   indentation = template[template.rfind("\n", 0, template.find(replace_text)) + 1 : template.find(replace_text)]
+
+   result = ""
+   lines = raw_text.split("\n")
+   for line_i in range(len(lines)):
+      result += ("" if line_i == 0 else indentation) + lines[line_i] + ("\n" if line_i != (len(lines) - 1) else "")
+
+   return result
+
+#HTML generation stuff
+def htmlNavBarTab(text, is_selected, style, link):
+   result =  '<div' + ((' style="' + style + '"') if len(style) > 0 else "") + ' class="nav_tab_div ' + ("nav_tab_selected" if is_selected else "nav_tab_not_selected") + '"' + ((" onclick=\"window.location.pathname='" + link + "'\"") if len(link) > 0 else "") + '>\n'
    result += '\t<a class="nav_tab_text">' + text + '</a>\n'
    result += '</div>\n'
    return result
 
-def htmlNavBar(pages):
-   pass
+def htmlNavBar(pages, current_page):
+   result = ""
+   for i in range(len(pages)):
+      page = pages[i]
+      result += htmlNavBarTab(page["title"], page["title"] == current_page, "border-top-left-radius: 5px" if i == 0 else "", "/generated/" + page["title"] + ".html")
+   return result[:-1] #NOTE: removes the trailing newline so it looks nicer
 
-# Load template.html
-# replace "REPLACE WITH NAV TABS" & "REPLACE WITH CONTENT"
+def generatePage(template_html, title, nav_tabs_raw, content_raw):
+   nav_tabs = prettyIndentation(template_html, "<!-- REPLACE WITH NAV TABS -->", nav_tabs_raw)
+   content = prettyIndentation(template_html, "<!-- REPLACE WITH CONTENT -->", content_raw)
 
-print(htmlNavBarTab("Home", True, "border-top-left-radius: 5px"))
+   return template_html.replace("<!-- REPLACE WITH TITLE -->", "<title>" + title + "</title>").replace("<!-- REPLACE WITH NAV TABS -->", nav_tabs).replace("<!-- REPLACE WITH CONTENT -->", content)
+
+#Page specification stuff
+def makePage(title, content_path):
+   return {"title": title, "content_path": content_path}
+
+template_html = readFile("template.html")
+pages = [makePage("Home", "content/home_content.html"), makePage("Sponsors", "content/home_content.html"), makePage("Blog", "content/blog_content.html"), makePage("Robots", "content/robot_content.html"), makePage("Gallery", "content/gallery_content.html")]
+
+for page in pages:
+   writeFile("generated/" + page["title"] + ".html", generatePage(template_html, "Newman Robotics - " + page["title"], htmlNavBar(pages, page["title"]), readFile(page["content_path"])))
